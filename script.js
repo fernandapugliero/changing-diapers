81d2005cf31835289efc00008db1ea56186be8ad
Fernanda Pugliero <51171043+fernandapugliero@users.noreply.github.com>
2025-04-11 18:45:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ fetch('places.json')
   .then(res => res.json())
   .then(data => {
     data.forEach(place => {
-      const marker = L.marker([place.latitude, place.longitude]).addTo(map);
+      const siteLine = place.site ? `<br><a href="\${place.site}" target="_blank">Visit site</a>` : '';
       const popupContent = `
-        <strong>${place.name}</strong><br>
-        ${place.address}<br>
-        Type: ${place.type}<br>
-        City: ${place.city}, Neighborhood: ${place.neighborhood}<br>
-        Changing Table: ${place.has_changing_table}<br>
-        Location: ${place.changing_table_location}<br>
-        Supplies: ${place.supplies_available.join(', ')}<br>
-        Conditions: ${place.conditions.join(', ')}
+        <strong>\${place.name}</strong><br>
+        \${place.address}<br>
+        Type: \${place.type}<br>
+        City: \${place.city}, Neighborhood: \${place.neighborhood}<br>
+        Changing Table: \${place.changing_table_location}<br>
+        Supplies: \${place.supplies_available.join(', ')}<br>
+        Conditions: \${place.conditions.join(', ')}<br>
+        Room for stroller: \${place.room_for_stroller ? 'Yes' : 'No'}
+        \${siteLine}
       `;
+      const marker = L.marker([place.latitude, place.longitude]).addTo(map);
       marker.bindPopup(popupContent);
     });
   });
